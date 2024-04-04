5e12115ae1f36d23bf07eef6871bef4f08bbe3b2
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2024-04-04 15:31:03 +0530

    Initial commit
    Upload files

---
+++ app/src/main/java/net/chitholian/potholesdetection/DetectorActivity.java
@@ -0,0 +1,325 @@
+
+package net.chitholian.potholesdetection;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Bundle;
+import android.util.Size;
+import android.widget.Toast;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.chitholian.potholesdetection.OverlayView.DrawCallback;
+import net.chitholian.potholesdetection.env.ImageUtils;
+import net.chitholian.potholesdetection.env.Logger;
+import net.chitholian.potholesdetection.tracking.MultiBoxTracker;
+
+import android.media.MediaPlayer;
+
+public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
+  private static final Logger LOGGER = new Logger();
+
+  private MediaPlayer mpSlow;
+  private MediaPlayer mpMed;
+  private MediaPlayer mpFast;
+
+  private static final int TF_OD_API_INPUT_SIZE = 300;
+  private static final boolean TF_OD_API_MODEL_QUANTIZED = true;
+  private static final String TF_OD_API_MODEL_FILE = "potholes.tflite";
+  private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/potholes.txt";
+
+  // Minimum detection confidence to track a detection.
+  private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+
+  private static final boolean MAINTAIN_ASPECT = false;
+
+  private static final Size DESIRED_PREVIEW_SIZE = new Size(480, 480);
+
+  private Integer sensorOrientation;
+
+  private Classifier detector;
+
+  private Bitmap rgbFrameBitmap = null;
+  private Bitmap croppedBitmap = null;
+  private Bitmap cropCopyBitmap = null;
+
+  private boolean computingDetection = false;
+
+  private long timestamp = 0;
+
+  private Matrix frameToCropTransform;
+  private Matrix cropToFrameTransform;
+
+  private MultiBoxTracker tracker;
+
+  private byte[] luminanceCopy;
+
+
+  @Override
+  public void onPreviewSizeChosen(final Size size, final int rotation) {
+
+    tracker = new MultiBoxTracker(this);
+
+    int cropSize = TF_OD_API_INPUT_SIZE;
+
+    try {
+      detector = TFLiteObjectDetectionAPIModel.create(
+              getAssets(), TF_OD_API_MODEL_FILE, TF_OD_API_LABELS_FILE, TF_OD_API_INPUT_SIZE, TF_OD_API_MODEL_QUANTIZED);
+      cropSize = TF_OD_API_INPUT_SIZE;
+    } catch (final IOException e) {
+      LOGGER.e(e, "Exception initializing classifier!");
+      Toast toast =
+              Toast.makeText(
+                      getApplicationContext(), "Classifier could not be initialized", Toast.LENGTH_SHORT);
+      toast.show();
+      finish();
+    }
+
+    previewWidth = size.getWidth();
+    previewHeight = size.getHeight();
+
+    sensorOrientation = rotation - getScreenOrientation();
+    LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation);
+
+    LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+    rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+    croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Config.ARGB_8888);
+
+    frameToCropTransform =
+            ImageUtils.getTransformationMatrix(
+                    previewWidth, previewHeight,
+                    cropSize, cropSize,
+                    sensorOrientation, MAINTAIN_ASPECT);
+
+    cropToFrameTransform = new Matrix();
+    frameToCropTransform.invert(cropToFrameTransform);
+
+    trackingOverlay = (OverlayView) findViewById(R.id.tracking_overlay);
+    trackingOverlay.addCallback(
+            new DrawCallback() {
+              @Override
+              public void drawCallback(final Canvas canvas) {
+                tracker.draw(canvas);
+                if (isDebug()) {
+                  tracker.drawDebug(canvas);
+                }
+              }
+            });
+
+    addCallback(
+            new DrawCallback() {
+              @Override
+              public void drawCallback(final Canvas canvas) {
+                if (!isDebug()) {
+                  return;
+                }
+                final Bitmap copy = cropCopyBitmap;
+                if (copy == null) {
+                  return;
+                }
+
+                final int backgroundColor = Color.argb(100, 0, 0, 0);
+                canvas.drawColor(backgroundColor);
+
+                final Matrix matrix = new Matrix();
+                final float scaleFactor = 2;
+                matrix.postScale(scaleFactor, scaleFactor);
+                matrix.postTranslate(
+                        canvas.getWidth() - copy.getWidth() * scaleFactor,
+                        canvas.getHeight() - copy.getHeight() * scaleFactor);
+                canvas.drawBitmap(copy, matrix, new Paint());
+              }
+            });
+  }
+
+  OverlayView trackingOverlay;
+
+  @Override
+  protected void processImage() {
+    ++timestamp;
+    final long currTimestamp = timestamp;
+    byte[] originalLuminance = getLuminance();
+    tracker.onFrame(
+            previewWidth,
+            previewHeight,
+            getLuminanceStride(),
+            sensorOrientation,
+            originalLuminance,
+            timestamp);
+    trackingOverlay.postInvalidate();
+
+    // No mutex needed as this method is not reentrant.
+    if (computingDetection) {
+      readyForNextImage();
+      return;
+    }
+    computingDetection = true;
+    LOGGER.i("Preparing image " + currTimestamp + " for detection in bg thread.");
+
+    rgbFrameBitmap.setPixels(getRgbBytes(), 0, previewWidth, 0, 0, previewWidth, previewHeight);
+
+    if (luminanceCopy == null) {
+      luminanceCopy = new byte[originalLuminance.length];
+    }
+    System.arraycopy(originalLuminance, 0, luminanceCopy, 0, originalLuminance.length);
+    readyForNextImage();
+
+    final Canvas canvas = new Canvas(croppedBitmap);
+    canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+
+    runInBackground(
+            new Runnable() {
+              @Override
+              public void run() {
+                LOGGER.i("Running detection on image " + currTimestamp);
+                final List<Classifier.Recognition> results = detector.recognizeImage(croppedBitmap);
+
+                cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                final Canvas canvas = new Canvas(cropCopyBitmap);
+                final Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                paint.setStyle(Style.STROKE);
+                paint.setStrokeWidth(2.0f);
+                paint.setTextSize(24);
+
+                final List<Classifier.Recognition> mappedRecognitions =
+                        new LinkedList<Classifier.Recognition>();
+
+                for (final Classifier.Recognition result : results) {
+                  final RectF location = result.getLocation();
+                  if (location != null && result.getConfidence() >= MINIMUM_CONFIDENCE_TF_OD_API) {
+                    float potholeSize = location.width() * location.height();
+                    canvas.drawRect(location, paint);
+                    LOGGER.i("Bounding Box Area: " + potholeSize);
+                    int numPotholes = tracker.trackedObjects.size();
+                    if (numPotholes == 0)
+                    {
+                      stopAllAlarms();
+                    }
+                    else {
+                      if (potholeSize > 13000) {
+                        mpSlow.pause();
+                        mpSlow.seekTo(0);
+                        mpMed.pause();
+                        mpMed.seekTo(0);
+                        playAlarm(mpFast);
+                      } else if (potholeSize > 7000 && potholeSize < 12000) {
+                        mpSlow.pause();
+                        mpSlow.seekTo(0);
+                        playAlarm(mpMed);
+                      } else {
+                        playAlarm(mpSlow);
+                      }
+                    }
+
+
+                    cropToFrameTransform.mapRect(location);
+                    result.setLocation(location);
+                    mappedRecognitions.add(result);
+                  }
+                }
+
+                tracker.trackResults(mappedRecognitions, luminanceCopy, currTimestamp);
+
+
+
+//                if (numPotholes == 0) {
+//                  stopAllAlarms();
+//                }
+//                else if (numPotholes == 1) {
+//                  playAlarm(mpSlow);
+//                } else if (numPotholes >= 2 && numPotholes <= 3) {
+//                  mpSlow.pause();
+//                  mpSlow.seekTo(0);
+//                  playAlarm(mpMed);
+//                } else {
+//                  mpSlow.pause();
+//                  mpSlow.seekTo(0);
+//                  mpMed.pause();
+//                  mpMed.seekTo(0);
+//                  playAlarm(mpFast);
+//                }
+
+
+                trackingOverlay.postInvalidate();
+
+                requestRender();
+                computingDetection = false;
+              }
+            });
+  }
+
+  @Override
+  protected int getLayoutId() {
+    return R.layout.camera_connection_fragment_tracking;
+  }
+
+  @Override
+  protected Size getDesiredPreviewFrameSize() {
+    return DESIRED_PREVIEW_SIZE;
+  }
+
+  @Override
+  public void onSetDebug(final boolean debug) {
+    detector.enableStatLogging(debug);
+  }
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mpSlow = MediaPlayer.create(this, R.raw.alarm_slow);
+    mpMed = MediaPlayer.create(this, R.raw.alarm_med);
+    mpFast = MediaPlayer.create(this, R.raw.alarm_fast);
+  }
+
+  @Override
+  public synchronized void onResume() {
+    stopAllAlarms();
+    super.onResume();
+  }
+
+  @Override
+  public synchronized void onStop() {
+    stopAllAlarms();
+    super.onStop();
+  }
+
+  @Override
+  public synchronized void onDestroy() {
+    stopAllAlarms();
+    mpSlow.release();
+    mpMed.release();
+    mpFast.release();
+    super.onDestroy();
+  }
+
+  private void playAlarm(MediaPlayer mediaPlayer) {
+    if (!mediaPlayer.isPlaying()) {
+      mediaPlayer.start();
+    }
+  }
+
+  private void stopAllAlarms() {
+    if (mpSlow.isPlaying()) {
+      mpSlow.pause();
+      mpSlow.seekTo(0);
+    }
+    if (mpMed.isPlaying()) {
+      mpMed.pause();
+      mpMed.seekTo(0);
+    }
+    if (mpFast.isPlaying()) {
+      mpFast.pause();
+      mpFast.seekTo(0);
+    }
+  }
+}
